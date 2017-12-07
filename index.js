cf76ddfbfccb3b6619cf99eef4ad03452e55ac04
midinpa <midinpa@gmail.com>
2017-12-07 18:29:17 +0900

    privatekey in buffer

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
 var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
 var Web3 = require("web3");
 
-function HDWalletProvider(private_key, provider_url) {
-  this.wallet = ethWallet.fromPrivateKey(private_key);
+function PrivateKeyWalletProvider(private_key, provider_url) {
+  this.wallet = ethWallet.fromPrivateKey(Buffer.from(private_key, 'hex'));
   this.address = "0x" + this.wallet.getAddress().toString("hex");
 
   this.engine = new ProviderEngine();
@@ -17,16 +17,16 @@ function HDWalletProvider(private_key, provider_url) {
   this.engine.start(); // Required by the provider engine.
 };
 
-HDWalletProvider.prototype.sendAsync = function() {
+PrivateKeyWalletProvider.prototype.sendAsync = function() {
   this.engine.sendAsync.apply(this.engine, arguments);
 };
 
-HDWalletProvider.prototype.send = function() {
+PrivateKeyWalletProvider.prototype.send = function() {
   return this.engine.send.apply(this.engine, arguments);
 };
 
-HDWalletProvider.prototype.getAddress = function() {
+PrivateKeyWalletProvider.prototype.getAddress = function() {
   return this.address;
 };
 
-module.exports = HDWalletProvider;
+module.exports = PrivateKeyWalletProvider;
